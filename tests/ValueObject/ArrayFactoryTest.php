e380a758f690f50ca4f0ecb9d16784a52b2c52c3
Sandro Keil <github@sandro-keil.de>
2021-08-27 10:12:16 +0200

    Add tests to not generate same code twice

--- tests/ValueObject/ArrayFactoryTest.php
+++ tests/ValueObject/ArrayFactoryTest.php
@@ -51,6 +51,11 @@ final class ArrayFactoryTest extends BaseTestCase
         $this->assertCode(
             $this->arrayFactory->nodeVisitorsFromNative('items', ...$type->items())
         );
+
+        $this->assertCode(
+            $this->arrayFactory->nodeVisitorsFromNative('items', ...$type->items()),
+            $this->getExpectedGeneratedCode()
+        );
     }
 
     /**
@@ -71,6 +76,11 @@ final class ArrayFactoryTest extends BaseTestCase
         $this->assertInstanceOf(ArrayType::class, $type);
 
         $this->assertCode($this->arrayFactory->nodeVisitors($type));
+
+        $this->assertCode(
+            $this->arrayFactory->nodeVisitors($type),
+            $this->getExpectedGeneratedCode()
+        );
     }
 
     /**
@@ -87,6 +97,13 @@ final class ArrayFactoryTest extends BaseTestCase
             $this->voFactory->nodeVisitors(
                 ArrayType::fromDefinition($definition)
             )
+        );
+
+        $this->assertCode(
+            $this->voFactory->nodeVisitors(
+                ArrayType::fromDefinition($definition)
+            ),
+            $this->getExpectedGeneratedCode()
         );
     }
 
@@ -106,14 +123,19 @@ final class ArrayFactoryTest extends BaseTestCase
         $this->assertCode(
             $classBuilder->generate($this->parser)
         );
+        $this->assertCode(
+            $classBuilder->generate($this->parser),
+            $this->getExpectedGeneratedCode()
+        );
     }
 
     /**
      * @param array<NodeVisitor> $nodeVisitors
+     * @param string $code
      */
-    private function assertCode(array $nodeVisitors): void
+    private function assertCode(array $nodeVisitors, string $code = '<?php final class ReasonTypeListVO {}'): void
     {
-        $ast = $this->parser->parse('<?php final class ReasonTypeListVO {}');
+        $ast = $this->parser->parse($code);
 
         $nodeTraverser = new NodeTraverser();
 
@@ -121,124 +143,127 @@ final class ArrayFactoryTest extends BaseTestCase
             $nodeTraverser->addVisitor($nodeVisitor);
         }
 
-        $expected = <<<'EOF'
-<?php
+        $this->assertSame($this->getExpectedGeneratedCode(), $this->printer->prettyPrintFile($nodeTraverser->traverse($ast)));
+    }
 
-final class ReasonTypeListVO implements \Iterator, \Countable
-{
-    private int $position = 0;
-    /**
-     * @var ReasonType[]
-     */
-    private array $items;
-    public function rewind() : void
+    private function getExpectedGeneratedCode(): string
     {
-        $this->position = 0;
-    }
-    public function current() : ReasonType
-    {
-        return $this->items[$this->position];
-    }
-    public function key() : int
-    {
-        return $this->position;
-    }
-    public function next() : void
-    {
-        ++$this->position;
-    }
-    public function valid() : bool
-    {
-        return isset($this->items[$this->position]);
-    }
-    public function count() : int
-    {
-        return count($this->items);
-    }
-    public static function fromArray(array $items) : self
-    {
-        return new self(...array_map(static function (string $item) {
-            return ReasonType::fromString($item);
-        }, $items));
-    }
-    public static function fromItems(ReasonType ...$items) : self
-    {
-        return new self(...$items);
-    }
-    public static function emptyList() : self
-    {
-        return new self();
-    }
-    private function __construct(ReasonType ...$items)
-    {
-        $this->items = $items;
-    }
-    public function add(ReasonType $reasonType) : self
-    {
-        $copy = clone $this;
-        $copy->items[] = $reasonType;
-        return $copy;
-    }
-    public function remove(ReasonType $reasonType) : self
-    {
-        $copy = clone $this;
-        $copy->items = array_values(array_filter($copy->items, static function ($v) use($reasonType) {
-            return !$v->equals($reasonType);
-        }));
-        return $copy;
-    }
-    public function first() : ?ReasonType
-    {
-        return $this->items[0] ?? null;
-    }
-    public function last() : ?ReasonType
-    {
-        if (count($this->items) === 0) {
-            return null;
-        }
-        return $this->items[count($this->items) - 1];
-    }
-    public function contains(ReasonType $reasonType) : bool
-    {
-        foreach ($this->items as $existingItem) {
-            if ($existingItem->equals($reasonType)) {
-                return true;
+        return <<<'EOF'
+        <?php
+        
+        final class ReasonTypeListVO implements \Iterator, \Countable
+        {
+            private int $position = 0;
+            /**
+             * @var ReasonType[]
+             */
+            private array $items;
+            public function rewind() : void
+            {
+                $this->position = 0;
+            }
+            public function current() : ReasonType
+            {
+                return $this->items[$this->position];
+            }
+            public function key() : int
+            {
+                return $this->position;
+            }
+            public function next() : void
+            {
+                ++$this->position;
+            }
+            public function valid() : bool
+            {
+                return isset($this->items[$this->position]);
+            }
+            public function count() : int
+            {
+                return count($this->items);
+            }
+            public static function fromArray(array $items) : self
+            {
+                return new self(...array_map(static function (string $item) {
+                    return ReasonType::fromString($item);
+                }, $items));
+            }
+            public static function fromItems(ReasonType ...$items) : self
+            {
+                return new self(...$items);
+            }
+            public static function emptyList() : self
+            {
+                return new self();
+            }
+            private function __construct(ReasonType ...$items)
+            {
+                $this->items = $items;
+            }
+            public function add(ReasonType $reasonType) : self
+            {
+                $copy = clone $this;
+                $copy->items[] = $reasonType;
+                return $copy;
+            }
+            public function remove(ReasonType $reasonType) : self
+            {
+                $copy = clone $this;
+                $copy->items = array_values(array_filter($copy->items, static function ($v) use($reasonType) {
+                    return !$v->equals($reasonType);
+                }));
+                return $copy;
+            }
+            public function first() : ?ReasonType
+            {
+                return $this->items[0] ?? null;
+            }
+            public function last() : ?ReasonType
+            {
+                if (count($this->items) === 0) {
+                    return null;
+                }
+                return $this->items[count($this->items) - 1];
+            }
+            public function contains(ReasonType $reasonType) : bool
+            {
+                foreach ($this->items as $existingItem) {
+                    if ($existingItem->equals($reasonType)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public function filter(callable $filter) : self
+            {
+                return new self(...array_values(array_filter($this->items, static function ($v) use($filter) {
+                    return $filter($v);
+                })));
+            }
+            /**
+             * @return ReasonType[]
+             */
+            public function items() : array
+            {
+                return $this->items;
+            }
+            public function toArray() : array
+            {
+                return \array_map(static function (ReasonType $reasonType) {
+                    return $reasonType->toString();
+                }, $this->items);
+            }
+            /**
+             * @param mixed $other
+             */
+            public function equals($other) : bool
+            {
+                if (!$other instanceof self) {
+                    return false;
+                }
+                return $this->toArray() === $other->toArray();
             }
         }
-        return false;
-    }
-    public function filter(callable $filter) : self
-    {
-        return new self(...array_values(array_filter($this->items, static function ($v) use($filter) {
-            return $filter($v);
-        })));
-    }
-    /**
-     * @return ReasonType[]
-     */
-    public function items() : array
-    {
-        return $this->items;
-    }
-    public function toArray() : array
-    {
-        return \array_map(static function (ReasonType $reasonType) {
-            return $reasonType->toString();
-        }, $this->items);
-    }
-    /**
-     * @param mixed $other
-     */
-    public function equals($other) : bool
-    {
-        if (!$other instanceof self) {
-            return false;
-        }
-        return $this->toArray() === $other->toArray();
-    }
-}
-EOF;
-
-        $this->assertSame($expected, $this->printer->prettyPrintFile($nodeTraverser->traverse($ast)));
+        EOF;
     }
 }
